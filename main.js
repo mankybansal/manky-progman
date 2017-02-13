f2d8ad6bf8f6f2d21fd584c4993f56d6d625d5b4
Mayank Bansal <sunny.bansal@gmail.com>
2017-02-13 12:33:41 -0600

    Added Directory Manifest Reader
    Errors still popping up.

--- main.js
+++ main.js
@@ -9,12 +9,36 @@ var ls = require('list-directory-contents');
 var express = require('express');
 var app = express();
 
-app.get('/listProjects', function (req, res) {
-  ls('projects', function (err, tree) {
-        console.log(tree);
+function readDirectory = function(parentDir,folder,callback){
+  var projects = [];
+  fs.readdir(parentDir + '/' + folder, (err, projectFiles) => {
+    projectFiles.forEach(projectFile => {
+        if(projectFile == "manifest.json"){
+          console.log("Found a Manifest: " + projectFile + " in projects/"+file);
+          project.manifest = projectFile;
+          console.log(project);
+          projects[projects.length] = project;
+        }
+    }, function(){
+      callback(projects);
+    });
   });
-  res.send();
-})
+}
+
+app.get('/listProjects', function (req, res) {
+
+
+  fs.readdir('projects', (err, files) => {
+    files.forEach(file => {
+        readDirectory('projects', file, function(data){
+          res.send(data);
+        })
+    });
+  });
+
+  res.send(projects);
+
+});
 
 https.createServer(options, app).listen(3000, function () {
 	console.log('Started!');
